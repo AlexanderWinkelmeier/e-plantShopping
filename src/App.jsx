ec7335ff26f60774ff606a2c3a082aa88b66820e
AlexanderWinkelmeier <r.a.winkelmeier@hotmail.de>
2025-06-27 18:56:29 +0200

    Verbessere die Benutzerinteraktion durch Optimierung des Home-Click-Handlers und Anpassung der Sichtbarkeit der Komponenten in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,44 +5,57 @@ import AboutUs from './AboutUs';
 
 function App() {
   const [showProductList, setShowProductList] = useState(false);
+  const [isTransitioning, setIsTransitioning] = useState(false);
 
   const handleGetStartedClick = () => {
+    setIsTransitioning(true);
     setShowProductList(true);
   };
 
   const handleHomeClick = () => {
     console.log('handleHomeClick wurde aufgerufen');
-    setShowProductList(false);
-    window.scrollTo(0, 0);
+    setIsTransitioning(true);
+    requestAnimationFrame(() => {
+      setShowProductList(false);
+      window.scrollTo(0, 0);
+      setTimeout(() => setIsTransitioning(false), 300);
+    });
   };
 
   return (
     <div className="app-container">
-      <div className={`landing-page ${showProductList ? 'fade-out' : ''}`}>
-        <div className="background-image"></div>
-        <div className="content">
-          <div className="landing_content">
-            <h1>Welcome To Paradise Nursery</h1>
-            <div className="divider"></div>
-            <p>Where Green Meets Serenity</p>
+      {(!showProductList || isTransitioning) && (
+        <div className={`landing-page ${showProductList ? 'fade-out' : ''}`}>
+          <div className="background-image"></div>
+          <div className="content">
+            <div className="landing_content">
+              <h1>Welcome To Paradise Nursery</h1>
+              <div className="divider"></div>
+              <p>Where Green Meets Serenity</p>
 
-            <button
-              className="get-started-button"
-              onClick={handleGetStartedClick}
-            >
-              Get Started
-            </button>
-          </div>
-          <div className="aboutus_container">
-            <AboutUs />
+              <button
+                className="get-started-button"
+                onClick={handleGetStartedClick}
+              >
+                Get Started
+              </button>
+            </div>
+            <div className="aboutus_container">
+              <AboutUs />
+            </div>
           </div>
         </div>
-      </div>
-      <div
-        className={`product-list-container ${showProductList ? 'visible' : ''}`}
-      >
-        <ProductList onHomeClick={handleHomeClick} />
-      </div>
+      )}
+      {(showProductList || isTransitioning) && (
+        <div
+          className={`product-list-container ${
+            showProductList ? 'visible' : ''
+          }`}
+          style={{ position: 'absolute', top: 0, left: 0, width: '100%' }}
+        >
+          <ProductList onHomeClick={handleHomeClick} />
+        </div>
+      )}
     </div>
   );
 }
