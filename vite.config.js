5c31211b2166ff2217e4abeaf6ac0de9f7d3058b
AlexanderWinkelmeier <r.a.winkelmeier@hotmail.de>
2025-06-27 17:46:05 +0200

    Bereinige die Basis-URL-Konfiguration in vite.config.js für die Produktionsumgebung

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base:
-    process.env.NODE_ENV === 'production'
-      ? '/AlexanderWinkelmeier/e-plantShopping/'
-      : '/',
+  base: process.env.NODE_ENV === 'production' ? '/e-plantShopping/' : '/',
   plugins: [react()],
 });
